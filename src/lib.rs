ff98cf7b8061c50fa6041d9bad957019f6671d80
Dylan Jessen <djessen@calpoly.edu>
2023-12-06 22:37:01 -0700

    various todos removed

--- src/lib.rs
+++ src/lib.rs
@@ -6,14 +6,14 @@ use std::collections::VecDeque;
 use anyhow::{Result, bail, ensure, anyhow};
 use std::cell::UnsafeCell;
 
-use opencv::features2d::BRISK;
-use opencv::core::{Mat, Vector, KeyPoint, Ptr, DMatch};
+use opencv::features2d::{self, BRISK, DrawMatchesFlags};
+use opencv::core::{Mat, Vector, KeyPoint, Ptr, DMatch, Scalar, NORM_HAMMING};
 use opencv::prelude::{MatTraitConst, Feature2DTrait};
 use opencv::videoio::{VideoCapture, CAP_ANY};
 use opencv::prelude::{VideoCaptureTrait, VideoCaptureTraitConst};
-use opencv::{imgcodecs, imgproc};
+use opencv::{imgcodecs, imgproc, highgui};
+use opencv::prelude::DescriptorMatcherTraitConst;
 
-// should only be for usize indexes, not entire Mats
 pub struct Stabilizer<T> {
     time_window: Duration,
     start_time: Option<Instant>,
@@ -55,6 +55,13 @@ impl<T> Stabilizer<T> {
             // stop expanding the queue
         }
 
+        // implement this whole thing as simply as possible:
+        // - queue size determined by how many frames can
+        //   be read and stuffed into the queue in X seconds.
+        // - return the item most frequently occuring in the
+        //   current queue.
+        //
+        // - T = &Frame (we do pointer comparison)
         todo!("add unstabilized item to the buffer") 
     }
     pub fn get(&self) -> Option<&T> {
@@ -62,12 +69,7 @@ impl<T> Stabilizer<T> {
     }
 }
 
-// Don't implement this. just do .next() once on `kebab::device`.
-//pub fn capture_one(device: usize) -> Mat {
-//    todo!("open a video device, but only to get the first frame")
-//}
-
-struct Features {
+pub struct Features {
     pub(crate) kps: Vector<KeyPoint>,
     pub(crate) dcs: Mat,
     pub(crate) algorithm: Ptr<BRISK>,
@@ -89,7 +91,7 @@ impl Default for Features {
 }
 
 impl Features {
-    fn calculate(&mut self, img: &Mat) -> Result<()> {
+    fn calculate(&mut self, img: &Mat) {
         let mask = Mat::default();
         self.algorithm.detect_and_compute(
             img,
@@ -97,60 +99,127 @@ impl Features {
             &mut self.kps,
             &mut self.dcs,
             false,
-        )?;
-        Ok(())
+        ).unwrap();
     }
 }
 
 pub struct Frame {
-    img: Mat,
+    mat: Mat,
     fts: UnsafeCell<Option<Features>>,
 }
 
 impl Frame {
-    fn from_img(img: Mat) -> Self {
+    fn from_mat(mat: Mat) -> Self {
         Self {
-            img: img,
+            mat: mat,
             fts: UnsafeCell::new(None),
         }
     }
-    pub(crate) fn features(&self) -> Result<&Features> {
+    pub(crate) fn features(&self) -> &Features {
         let fts = unsafe { & *self.fts.get() };
         if let Some(fts) = fts {
-            return Ok(&fts);
+            return fts;
         };
         let mut tmp = Features::default();
-        tmp.calculate(&self.img)?;
+        tmp.calculate(&self.mat);
         let fts = unsafe { &mut *self.fts.get() };
         *fts = Some(tmp);
-        Ok(fts.as_ref().unwrap())
+        fts.as_ref().unwrap()
     }
+}
+
+pub fn features(frame: &Frame) -> &Features {
+    frame.features()
 }
 
 pub struct Matches<'a> {
     matches: Vector<DMatch>,
-    train_frame: &'a Frame,
-    query_frame: &'a Frame,
+    train: &'a Frame,
+    query: &'a Frame,
 }
 
 pub fn matches<'a>(train: &'a Frame, query: &'a Frame, threshold: f32) -> Matches<'a> {
-    todo!("copy-paste from yenndo vision")    
+//fn brute_force_match(query_dcs: &Mat, train_dcs: &Mat, threshold: f32) -> Result<Vector<DMatch>> {
+    let mut all_matches = Vector::new();
+    let mut good_matches = Vector::new();
+
+    // KNN=2 Match
+    let bf_matcher = opencv::features2d::BFMatcher::create(NORM_HAMMING, false).unwrap();
+    bf_matcher.knn_train_match(
+        &query.features().dcs,
+        &train.features().dcs,
+        &mut all_matches,
+        2,
+        &Mat::default(),
+        false,
+    ).unwrap();
+
+    // Filter
+    for pair_dmatch in all_matches {
+        let m = pair_dmatch.get(0).expect("pair_dmatch.get(0)");
+        let n = pair_dmatch.get(1).expect("pair_dmatch.get(1)");
+        if m.distance < threshold * n.distance {
+            good_matches.push(m);
+        }
+    }
+    Matches {
+        matches: good_matches,
+        train: train,
+        query: query,
+    }
 }
 
-pub fn show(frame: &Frame) {
-    todo!("copy-paste from yenndo vision")    
+pub fn window(window: &str) {
+    highgui::named_window(window, highgui::WINDOW_NORMAL).unwrap();
+}
+
+pub fn show(frame: &Frame, window: &str) {
+    if let Err(_) = highgui::imshow(window, &frame.mat) {
+        eprintln!("show(): window does not exist: {}", window)
+    }
+}
+
+pub fn close(window: &str) {
+    let _ = highgui::destroy_window(window);
+}
+
+pub fn wait_key(code: i32) -> bool {
+    highgui::wait_key(1).unwrap() == code
 }
 
 pub fn draw_features(frame: &Frame) -> Frame {
-    todo!("copy-paste from yenndo vision")    
+//pub fn draw_keypoints(img: &Mat, kps: &Vector<KeyPoint>) -> Result<Mat> {
+    let mut out = Mat::default();
+    features2d::draw_keypoints(
+        &frame.mat,
+        &frame.features().kps,
+        &mut out,
+        Scalar::all(-1.0),
+        DrawMatchesFlags::DEFAULT,
+    ).unwrap();
+    Frame::from_mat(out)
 }
 
 pub fn draw_matches(matches: &Matches) -> Frame {
-    todo!("copy-paste from yenndo vision")    
+    let Matches { matches, query, train } = matches;
+
+    let mut out = Mat::default();
+    features2d::draw_matches(
+        &query.mat,
+        &query.features().kps,
+        &train.mat,
+        &query.features().kps,
+        &matches, 
+        &mut out,
+        Scalar::all(-1.0),
+        Scalar::all(-1.0),
+        &Vector::default(),
+        features2d::DrawMatchesFlags::DEFAULT,
+    ).unwrap();
+    Frame::from_mat(out)
 }
 
-
-pub fn device(index: i32) -> impl Iterator<Item = Frame> {
+pub fn device(index: i32) -> impl Iterator<Item=Frame> {
     std::iter::from_generator(move || {
         let mut cap = VideoCapture::new(index, CAP_ANY).unwrap();
         let Ok(is_opened) = cap.is_opened() else {
@@ -171,12 +240,12 @@ pub fn device(index: i32) -> impl Iterator<Item = Frame> {
             if img_size.width == 0 || img_size.height == 0 {
                 break;
             }
-            yield Frame::from_img(img);
+            yield Frame::from_mat(img);
         }
     })
 }
 
-pub fn file(path: &str) -> impl Iterator<Item = Frame> {
+pub fn file(path: &str) -> impl Iterator<Item=Frame> {
     let path = path.to_string();
     std::iter::from_generator(move || {
         let Ok(mut cap) = VideoCapture::from_file(&path, CAP_ANY) else {
@@ -193,7 +262,7 @@ pub fn file(path: &str) -> impl Iterator<Item = Frame> {
                 eprintln!("file({}) -> img_to_gray() -> Err", path);
                 return;
             };
-            yield Frame::from_img(img);
+            yield Frame::from_mat(img);
             return;
         }
         loop {
@@ -210,7 +279,7 @@ pub fn file(path: &str) -> impl Iterator<Item = Frame> {
                 eprintln!("file({}) -> img_to_gray() -> Err", path);
                 return;
             };
-            yield Frame::from_img(img);
+            yield Frame::from_mat(img);
         }
     })
 }
@@ -220,23 +289,5 @@ fn img_to_gray(img: Mat) -> Result<Mat> {
     imgproc::cvt_color(&img, &mut gray_img, imgproc::COLOR_BGR2GRAY, 0)?;
     Ok(gray_img)
 }
-
-
-
-//pub fn features(mat: Mat) -> BriskFeatures {
-//    todo!("return BRISK features for the mat")
-//}
-//
-//pub fn most_similar(frame: BriskFeatures, options: &[BriskFeatures]) -> usize {
-//    todo!("return most similar index")
-//}
-//
-//pub fn show(mat: Mat) {
-//
-//}
-
-// and also something for an image/video display loop.
-// (at least an image display)
-
 
 
